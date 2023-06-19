fc918e69c89a3acc7aec435794babe786ce2fa0b
I4ir0 <ichigo2805@gmail.com>
2023-06-19 21:37:10 +0300

    beta1.1

--- Lesson2/Lesson2.cpp
+++ Lesson2/Lesson2.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
     std::cout << "Hello Git!" << std::endl;
+	for (size_t i = 0; i < 15; i++)
+	{
+		std::cout << "Hello Git!" << std::endl;
+	}
     return 0;
 }
